fd79980087c8b7eb4456dafc1148c31506808900
smit-finder <smit.r.patel5@gmail.com>
2024-05-17 12:52:55 -0400

    Stored application in database

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
+
 
 app = Flask(__name__)
 
@@ -22,7 +23,17 @@ def show_job(id):
         return "Not Found", 404
     return render_template('jobpage.html', job=job)
 
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+    data = request.form
+    job = load_job_from_db(id)
+
+    add_application_to_db(id, data)
+    # send an email
+    # display an acknowledgement
+    return render_template('application_submitted.html',
+                           application=data,
+                           job=job)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
-    host = os.environ.get('DB_HOST')
-    print(host)
--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import pymysql
 from sqlalchemy import create_engine, text
 import os
+from sqlalchemy.exc import SQLAlchemyError
+
 
 def get_database_engine():
     # Retrieve database connection parameters from environment variables
@@ -50,5 +52,25 @@ def load_job_from_db(id):
         row_dict = dict(rows[0])
         return row_dict
 
+def add_application_to_db(job_id, data):
+    engine = get_database_engine()
+
+    with engine.connect() as conn:
+        query = text("insert into applications (job_id, full_name, "
+                     "email, linkedin_url, education, work_experience, "
+                     "resume_url) VALUES(:job_id, :full_name, :email, "
+                     ":linkedin_url, :education, :work_experience, :resume_url)")
+
+        values = {'job_id': job_id,
+                  'full_name': data['full_name'],
+                  'email': data['email'],
+                  'linkedin_url': data['linkedin_url'],
+                  'education': data['education'],
+                  'work_experience': data['work_experience'],
+                  'resume_url': data['resume_url']
+                  }
+
+        conn.execute(query, values)
+        conn.commit()
 
 
